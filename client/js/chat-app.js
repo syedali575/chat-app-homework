22bc82abdc1f62989d19492b85e44fc936287a51
Syed Ali <sali3@cox.net>
2016-11-02 14:52:42 -0400

    Finall ajax is working, I am able to reach server

--- client/js/chat-app.js
+++ client/js/chat-app.js
@@ -1,20 +1,39 @@
 (function(){
+
+window.form = window.form || {};
+
 "use strict";
 
 
 
 
+$(".login")
+  .on("submit", function initiate(event){
+    event.preventDefault();
+    // console.log("submit button working");
+
+    var uname = $(".username").val();
 
 
+    $.ajax({
+      url: "/login",
+      method: "POST",
+      data: JSON.stringify({ username: uname }),
+      headers: {
+        "Content-Type": "application/json"
+      },
+    })
+
+    .done(function handleSuccess(success){
+      console.log("Worked");
+    })
+
+    .fail(function notWorking(xhr){
+      console.log(xhr,"Unable to communicate");
+
+    });
 
 
-
-
-
-
-
-
-
-
+  });
 
 })();
